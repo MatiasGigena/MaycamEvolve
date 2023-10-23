7ea3997525a0db1521f51422cf0de419c0ac79c3
Matias Gigena <tute910@gmail.com>
2023-10-23 12:43:31 -0300

    timing

--- app/(secciones)/main/page.jsx
+++ app/(secciones)/main/page.jsx
@@ -13,7 +13,7 @@ const MainPage = () => {
       setIsLoading(false);
       document.body.style.cursor = 'default';
       router.push('/main');
-    }, 5200);
+    }, 5600);
   }, [router]);
 
   return (
