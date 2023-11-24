a128a8de9b80dc765692a95594c94e97e901b1f3
Matias Gigena <tute910@gmail.com>
2023-11-24 12:05:19 -0300

    barra

--- app/(secciones)/layout.jsx
+++ app/(secciones)/layout.jsx
@@ -6,32 +6,26 @@ import { TfiWorld } from 'react-icons/tfi';
 const Layout = ({ children }) => {
   return (
     <main className='bg-[#1a1e1c] flex flex-col fondo3D '>
-      <div className='bg-[#715bd4] flex gap-4 pr-3 lg:pr-0 text-white'>
-        <marquee
-          className='text-white'
-          bgcolor='#715bd4'
-          direction='left'
-          scrollamount='8'
-          scrolldelay='40'
-          truespeed='true'
-          width='80%'
-        >
-          <div className='flex items-center justify-center gap-2'>
+      <section className='bg-[#715bd4] flex gap-4 pr-3 lg:pr-0 text-white'>
+        <div className='text-white text-wrapper'>
+          <div className='flex items-center justify-center gap-2 '>
             <AiOutlineWarning />
             <p>
               Aca colocarias info importante cualquier cosa
               que hay que poner se va a mostrar aca
             </p>
           </div>
-        </marquee>
-        <nav className='flex items-center gap-3'>
+        </div>
+        <nav className='flex items-center gap-2'>
           <div className='flex items-center gap-2'>
             <strong>Argentina</strong>
             <TfiWorld />
           </div>
-          <a href='/contacto'>Ayuda</a>
+          <a className='cursor-pointer2' href='/contacto'>
+            Ayuda
+          </a>
         </nav>
-      </div>
+      </section>
       <section className='lg:pt-10 lg:pb-16'>
         <Navbar />
         {children}
